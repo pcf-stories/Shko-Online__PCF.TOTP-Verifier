801d9e4f7e5502a8c84f437932dbed473baa7b2c
asllanmakaj7 <animakaj7@gmail.com>
2024-03-18 15:24:22 +0100

    added theme

---
+++ .storybook/YourTheme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'TOTP-Verifier',
+  brandUrl: 'https://github.com/Shko-Online/PCF.TOTP-Verifier',
+  brandImage: './Shko Online 92x32.svg',
+  brandTarget: '_self',
+});
